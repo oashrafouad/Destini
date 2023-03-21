e681be69be002b685207b536b6cb67f346cdf856
Omar Ashraf <amourakora2001@gmail.com>
2023-03-21 16:14:24 +0200

    changed the label to display the first story upon launch

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.text = story0
 
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
 }
 
